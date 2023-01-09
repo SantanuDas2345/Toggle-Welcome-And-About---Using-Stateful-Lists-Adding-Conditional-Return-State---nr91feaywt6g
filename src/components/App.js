64f03e86005096c5c11ff3c9940049f34367aef9
SantanuDas2345 <90392244+SantanuDas2345@users.noreply.github.com>
2023-01-10 00:37:59 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,16 @@
-import React from "react";
+import React, {useState} from "react";
 import "../styles/App.css";
 
 const App = () => {
+  const[data, setData] = useState(true);
+  
   return (
     <div id="main">
+    {data ? (
       <div id="welcome-div">
         <h1>Welcome to Newton School!!</h1>
       </div>
+    ) :(
       <div id="about-div">
         <h1>
           Newton School is a neo-university providing highly immersive tech
@@ -14,7 +18,8 @@ const App = () => {
           opportunities.
         </h1>
       </div>
-      <button id="toggle">Toggle</button>
+    )}
+      <button id="toggle" onClick={()=>setData(!data)}>Toggle</button>
     </div>
   );
 };
